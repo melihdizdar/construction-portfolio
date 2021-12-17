daf05f7d00f4d69374415c00ce7f62e8b179fee1
Melih <mlhdzdr@gmail.com>
2021-12-17 15:49:38 +0300

    add topbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopBar from "./components/TopBar/TopBar";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div>
         <BrowserRouter>
+            <TopBar/>
             <main>
                 <Routes>
                     <Route path="/" element={<HomeScreen/>}/>
